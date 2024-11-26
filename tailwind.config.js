f1c1dd3eadb0ea0ed55105a7b4e5f086c3667158
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-11-26 11:35:16 +0530

    setup own theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seasalt: "#F8FAFC",
+        mediumslateblue: "#726DD1",
+        secondary: "#E1E7FE",
+        primary: "#4F44E5",
+        oxfordblue: "#171B2D",
+        battleshipgray: "#888D93",
+      },
+    },
   },
   plugins: [],
 };
