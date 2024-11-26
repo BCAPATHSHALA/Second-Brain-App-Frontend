b32e2811b7802ee46d97581d01b4c8eebe4df675
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-11-26 13:14:32 +0530

    create Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { PlusIcon, ShareIcon } from "@heroicons/react/16/solid";
+import Button from "./components/ui/button";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex gap-4">
+      <Button
+        title="Create New Content"
+        startIcon={<ShareIcon className="w-4 h-4" />}
+        variant="secondary"
+        size="md"
+      />
+      <Button
+        title="Create New Content"
+        startIcon={<PlusIcon className="w-4 h-4" />}
+        variant="primary"
+        size="md"
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,40 @@
+import { ReactElement } from "react";
+
+interface ButtonInterface {
+  title: string;
+  size: "lg" | "sm" | "md";
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
+  variant: "primary" | "secondary";
+}
+
+const sizeStyles = {
+  lg: "px-8 py-4 text-xl",
+  md: "px-4 py-2 text-md",
+  sm: "px-2 py-1 text-sm",
+};
+
+const variantStyles = {
+  primary: "bg-primary text-seasalt",
+  secondary: "bg-secondary text-mediumslateblue",
+};
+
+const Button = ({
+  title,
+  size,
+  startIcon,
+  endIcon,
+  variant,
+}: ButtonInterface) => {
+  return (
+    <button
+      className={`flex justify-center items-center font-light rounded-md ${sizeStyles[size]} ${variantStyles[variant]}`}
+    >
+      {startIcon}
+      <p className={`pl-2  pr-2`}>{title}</p>
+      {endIcon}
+    </button>
+  );
+};
+
+export default Button;
